ef04546980eba13a72fca586049114d617ff8d09
zeuscod <42487273+zeuscod@users.noreply.github.com>
2021-06-01 09:33:49 -0500

    Update myapp.js

--- myapp.js
+++ myapp.js
@@ -1,7 +1,7 @@
 var apiObj = null;
 
 function StartMeeting() {
-    const domain = '8x8.vc';
+    const domain = 'meet.jit.si';
     const options = {
         roomName: 'JitsiMeetAPIExample',
         width: '100%',

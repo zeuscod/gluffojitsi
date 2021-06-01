cc9dacf963acb52a02c45ea675c5619f2bf5ff05
zeuscod <42487273+zeuscod@users.noreply.github.com>
2021-06-01 07:51:56 -0500

    Add files via upload

---
+++ myapp.js
@@ -0,0 +1,41 @@
+var apiObj = null;
+
+function StartMeeting() {
+    const domain = 'meet.jit.si';
+    const options = {
+        roomName: 'JitsiMeetAPIExample',
+        width: '100%',
+        height: '100%',
+        parentNode: document.querySelector('#jitsi-meet-conf-container'),
+        userInfo: {
+            displayName: 'Bilal Shahzad'
+        },
+        configOverwrite: {
+
+        },
+        interfaceConfigOverwrite: {
+            DISPLAY_WELCOME_PAGE_CONTENT: false,
+            TOOLBAR_BUTTONS: [
+                'microphone', 'camera'
+            ],
+        },
+        onload: function() {
+            alert('loaded');
+        }
+    };
+
+    apiObj = new JitsiMeetExternalAPI(domain, options);
+
+    apiObj.addEventListeners({
+        readyToClose: function() {
+            alert('going to close');
+            $("#jitsi-meet-conf-container").empty();
+        }
+    });
+
+    apiObj.executeCommand('subject', '');
+}
+
+function HangupCall() {
+    apiObj.executeCommand('hangup');
+}
